22c823a56073757963c3b5393170caa3b503a8dc
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-06 10:01:19 -0800

    Added api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+
+router.get("/api/workouts", () => {
+
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+
+});
+
+router.post("/api/workouts", (req, res) => {
+
+
+});
+
+router.get("/api/workouts/range", (req, res) => {
+
+});
+
+module.exports = router;
