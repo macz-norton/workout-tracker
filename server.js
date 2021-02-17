5d7a126351d84c52b1962480fa1e755e6927435c
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-17 08:55:45 -0800

    Added connection to mongoose

--- server.js
+++ server.js
@@ -2,4 +2,18 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
+
+app.use(logger("dev"));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Connection to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true});
+
