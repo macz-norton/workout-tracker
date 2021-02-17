d6ddde4921fa611b519e20dd5de46d639cd5d13f
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-17 09:18:27 -0800

    Added req. res to API get route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 
-router.get("/api/workouts", () => {
+router.get("/api/workouts", (req, res) => {
 
 });
 
@@ -9,7 +9,6 @@ router.put("/api/workouts/:id", (req, res) => {
 });
 
 router.post("/api/workouts", (req, res) => {
-
 
 });
 
