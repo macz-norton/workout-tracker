2d2d26e9d78df2e6ed3c72d9773602dfc3972a00
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-17 22:33:13 -0800

    Added get and post routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,18 @@
 const router = require("express").Router();
 
+const { Workout } = require("../models");
+
 router.get("/api/workouts", (req, res) => {
+
+    Workout.find().then((workouts) => {
+
+        res.json(workouts);
+
+    }).catch((err) => {
+
+        res.status(500).json(err);
+
+    });
 
 });
 
@@ -9,6 +21,16 @@ router.put("/api/workouts/:id", (req, res) => {
 });
 
 router.post("/api/workouts", (req, res) => {
+
+    Workout.create(req.body).then((workout) => {
+
+        res.json(workout);
+
+    }).catch((err) => {
+
+        res.status(500).json(err);
+
+    });
 
 });
 
