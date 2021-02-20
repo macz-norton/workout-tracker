2c12d9dd4fe06534b13ddb56a6226a3259c578e6
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-20 07:57:36 -0800

    Re-organized API routes and added default value to date

--- models/Workout.js
+++ models/Workout.js
@@ -3,11 +3,14 @@ const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema ({
 
-    day: Date,
+    day: {
+        type: Date,
+        default: Date.now
+    },
     exercises:[
         {
             // add type,name,duration required + add other validation
-            // default value for day
+            // default day.now value for day
             type: String,
             name: String,
             duration: Number,
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -15,25 +15,7 @@ router.get("/api/workouts", (req, res) => {
 
 });
 
-router.put("/api/workouts/:id", (req, res) => {
-
-    const newExercise = req.body;
-
-    Workout.findByIdAndUpdate(req.params.id, {$push: {
-
-        exercises: newExercise;
-
-    }});
-
-});
-
 router.post("/api/workouts", (req, res) => {
-
-    const newWorkout = {
-        day: Day.now(),
-        exercises: [req.body]
-    }
-    
     
     Workout.create(newWorkout).then((workout) => {
 
@@ -46,6 +28,20 @@ router.post("/api/workouts", (req, res) => {
     });
 
 });
+
+router.put("/api/workouts/:id", (req, res) => {
+
+    const newExercise = req.body;
+
+    Workout.findByIdAndUpdate(req.params.id, {$push: {
+
+        exercises: newExercise
+
+    }}).then
+
+});
+
+
 
 router.get("/api/workouts/range", (req, res) => {
 
