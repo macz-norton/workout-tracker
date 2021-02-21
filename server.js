38b5c19663169aca09c86cf9c4b54506963388b7
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-20 20:17:25 -0800

    Updated mongoose.connect

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connection to mongoose
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true});
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout", 
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    });
 
 app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes"));
